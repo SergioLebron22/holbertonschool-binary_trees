6fa261f9ef9267eea1c3c22cedfba2cf9be1c312
SergioLebron22 <lebronsergio22@gmail.com>
2024-04-03 20:01:26 -0400

    fixed betty issues in postorder traversal function

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,17 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * 
+ * binary_tree_postorder - will go through the binary in postorder traversal
+ *
+ * @tree: tree being traversed
+ * @func: function pointer
 */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
+	if (tree == NULL || func == NULL)
 	{
 		return;
 	}
-	
+
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	func(tree->n);
 }
