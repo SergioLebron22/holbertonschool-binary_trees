00cf4e5ab7c13dff61cae496b37eed02e0ff5ea9
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-04-04 22:20:33 -0400

    Adding binary_tree_height, binary_tree_nodes, binary_tree_is_full, and binary_tree_sibling.c

--- 11-main.c
+++ 11-main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
@@ -8,6 +10,7 @@
 int main(void)
 {
     binary_tree_t *root;
+    size_t size;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
@@ -15,6 +18,12 @@ int main(void)
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
     binary_tree_print(root);
-    binary_tree_delete(root);
+
+    size = binary_tree_size(root);
+    printf("Size of %d: %lu\n", root->n, size);
+    size = binary_tree_size(root->right);
+    printf("Size of %d: %lu\n", root->right->n, size);
+    size = binary_tree_size(root->left->right);
+    printf("Size of %d: %lu\n", root->left->right->n, size);
     return (0);
 }
